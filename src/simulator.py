a797588ebd6758ea29e9a868c319f76513c8a42c
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2020-10-23 00:27:38 +0200

    Cleaned up somewhat

--- src/simulator.py
+++ src/simulator.py
@@ -1,13 +1,12 @@
 from random import randint, random as r
-import time
 
 batch_size = 50
 lot = 2000
 fail_limit = 200
 
-not_found_pct = 0.011
-connection_fail_pct = 0.002
-verification_fail_pct = 0.025
+NOT_FOUND_PCT = 0.011
+CONNECTION_FAIL_PCT = 0.002
+VERIFICATION_FAIL_PCT = 0.025
 
 
 def fc(batch, pct):
@@ -36,8 +35,6 @@ def start_simulation():
           "verification_failed, failed, ok, failures %")
     while keep_going:
         tick += 1
-        if tick % 20 == 0:
-            time.sleep(0)
         try:
             in_process = sum(deployed) + sum(not_found) + sum(conn_fail) + sum(
                 verify_fail)
@@ -50,23 +47,24 @@ def start_simulation():
                 deployed.insert(0, batch - randint(0, 2))
                 remaining -= batch
             # Now, see if some of the CPEs can be moved along
+            # TODO Need a better method to simulate continuous processing
             if deployed and (tick % 5 == 0 or len(deployed) > 3):
                 # Every 5 minutes we see if the "deployed" can be moved along.
                 verify_batch = deployed.pop()
-                nf = fc(verify_batch, not_found_pct)
-                cf = fc(verify_batch - nf, connection_fail_pct)
-                vf = fc(verify_batch - nf - cf, verification_fail_pct)
+                nf = fc(verify_batch, NOT_FOUND_PCT)
+                cf = fc(verify_batch - nf, CONNECTION_FAIL_PCT)
+                vf = fc(verify_batch - nf - cf, VERIFICATION_FAIL_PCT)
                 ok += verify_batch - nf - cf - vf
                 not_found.insert(0, nf)
                 conn_fail.insert(0, cf)
                 verify_fail.insert(0, vf)
-            if tick % 3 == 0:
+            if tick % 2 == 0:
                 failed += not_found.pop() if not_found else 0
                 failed += conn_fail.pop() if conn_fail else 0
                 failed += verify_fail.pop() if verify_fail else 0
 
             fail_pct = failed * 100 / (failed + ok) if (failed + ok) else 0
-            print((f"Iteration {tick}, {sum( deployed )}, "
+            print((f"{tick}, {sum( deployed )}, "
                    f"{sum( not_found )}, "
                    f"{sum( conn_fail )}, "
                    f"{sum( verify_fail )}, "
@@ -74,7 +72,7 @@ def start_simulation():
                    f"{fail_pct:0.1f}%"))
             if remaining <= 0 and (not deployed and not not_found and not
                                    conn_fail and not verify_fail):
-                print("Queue is empty. no remaining")
+                print("Queue is empty. No remaining items to process.")
                 keep_going = False
 
         except KeyboardInterrupt:
