5eb5e885a511d07930c00ffc34428f903b18f9d8
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2020-10-27 10:36:48 +0100

    Adapted to run many simulations.

--- src/simulator.py
+++ src/simulator.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from random import randint, random as r
 
-batch_size = 50
-lot = 2000
-fail_limit = 200
-
 NOT_FOUND_PCT = 0.011
 CONNECTION_FAIL_PCT = 0.002
-VERIFICATION_FAIL_PCT = 0.025
+VERIFICATION_FAIL_PCT = 0.023
 
 
 def fc(batch, pct):
@@ -18,35 +14,33 @@ def fc(batch, pct):
     return res
 
 
-def start_simulation():
+def start_simulation(lot, batch, limit):
+    """
+    Perform a simulation.
+    """
     keep_going = True
     tick = 0
     ok = 0
     failed = 0
     remaining = lot
+    message = None
     # Handle these as queues
     conn_fail = []
     verify_fail = []
     deployed = []
     not_found = []
-    print(f"Simulation using fail limit={fail_limit}, batch size={batch_size}")
-    print(f"Running with {lot} CPEs to process")
-    print()
-    print("Iteration, deployed, not_found, connection_failed, "
-          "verification_failed, failed, ok, failures %")
     while keep_going:
         tick += 1
         try:
             in_process = sum(deployed) + sum(not_found) + sum(conn_fail) + sum(
                 verify_fail)
-            if in_process + failed < fail_limit and remaining > 0:
+            if in_process + failed < limit and remaining > 0:
                 # We can add more to the list
-                batch = batch_size if remaining > batch_size else remaining
+                batch = batch if remaining > batch else remaining
                 deployed.insert(0, batch - randint(0, 2))
                 remaining -= batch
             # Now, see if some of the CPEs can be moved along
-            # TODO Need a better method to simulate continuous processing
-            if deployed and (tick % 5 == 0 or len(deployed) > 3):
+            if (deployed and tick > 5) or len(deployed) == 5:
                 # Every 5 minutes we see if the "deployed" can be moved along.
                 verify_batch = deployed.pop()
                 nf = fc(verify_batch, NOT_FOUND_PCT)
@@ -61,27 +55,35 @@ def start_simulation():
                 failed += conn_fail.pop() if conn_fail else 0
                 failed += verify_fail.pop() if verify_fail else 0
 
-            fail_pct = failed * 100 / (failed + ok) if (failed + ok) else 0
-            print((f"{tick}, {sum( deployed )}, "
-                   f"{sum( not_found )}, "
-                   f"{sum( conn_fail )}, "
-                   f"{sum( verify_fail )}, "
-                   f"{failed}, {ok}, "
-                   f"{fail_pct:0.1f}%"))
             if remaining <= 0 and (not deployed and not not_found and not
                                    conn_fail and not verify_fail):
-                print("Queue is empty. No remaining items to process.")
+                message = "Queue is empty. No remaining items to process."
                 keep_going = False
-            if failed >= fail_limit:
-                print("Too many failed, stop the process!")
+            if failed >= limit:
+                message = "Too many failed. Stop the process!"
                 keep_going = False
 
         except KeyboardInterrupt:
             keep_going = False
 
+    fail_pct = failed * 100 / (failed + ok) if (failed + ok) else 0
+    return ((f"{lot}, {limit}, {batch}, "
+             f"{tick}, {sum( deployed )}, "
+             f"{sum( not_found )}, "
+             f"{sum( conn_fail )}, "
+             f"{sum( verify_fail )}, "
+             f"{failed}, {ok}, "
+             f"{fail_pct:0.1f}%, {message}"))
+
 
 if __name__ == '__main__':
     print("Starting simulation")
-    print("Each line represents status at 1 minute intervals.")
+    print("Each line represents status end of simulation.")
 
-    start_simulation()
+    print("Max_upgrades, max_failures, batch_size, "
+          "Iteration, deployed, not_found, connection_failed, "
+          "verification_failed, failed, ok, failures %, message")
+    for lot in [1000, 2000, 5000]:
+        for limit in [50, 100, 200]:
+            for batch in range(10, limit * 2 + 1, 10):
+                print(start_simulation(lot, batch, limit))
